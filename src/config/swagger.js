773c6d59352cd0924db82aea9e4407e26c25fcc9
gasanamous <162181627+gasanamous@users.noreply.github.com>
2025-06-29 19:31:40 +0300

    Update swagger.js

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -7,13 +7,13 @@ import YAML from 'yamljs';
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-const mainDocs = YAML.load(path.join(__dirname, '../docs/mainDocs.yaml'))
-const tableDocs = YAML.load(path.join(__dirname, '../docs/tableDocs.yaml'))
-const menuDocs = YAML.load(path.join(__dirname, '../docs/menuDocs.yaml'))
-const appReviewDocs = YAML.load(path.join(__dirname, '../docs/appReviewDocs.yaml'))
-const orderDocs = YAML.load(path.join(__dirname, '../docs/orderDocs.yaml'))
-const managerDocs = YAML.load(path.join(__dirname, '../docs/managerDocs.yaml'))
-const sessionDocs = YAML.load(path.join(__dirname, '../docs/sessionDocs.yaml'))
+const mainDocs = YAML.load(path.join(__dirname, '../Docs/mainDocs.yaml'))
+const tableDocs = YAML.load(path.join(__dirname, '../Docs/tableDocs.yaml'))
+const menuDocs = YAML.load(path.join(__dirname, '../Docs/menuDocs.yaml'))
+const appReviewDocs = YAML.load(path.join(__dirname, '../Docs/appReviewDocs.yaml'))
+const orderDocs = YAML.load(path.join(__dirname, '../Docs/orderDocs.yaml'))
+const managerDocs = YAML.load(path.join(__dirname, '../Docs/managerDocs.yaml'))
+const sessionDocs = YAML.load(path.join(__dirname, '../Docs/sessionDocs.yaml'))
 
 mainDocs.paths = {
     ...mainDocs.paths,
